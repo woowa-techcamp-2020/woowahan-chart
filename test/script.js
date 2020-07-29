f444040e8bae1c5fbd024a17f2e7f445ce319416
jhaemin <io@jhaemin.com>
2020-07-29 16:51:08 +0900

    test: line chart

--- test/script.js
+++ test/script.js
@@ -1,14 +1,50 @@
-import { Pie } from './lib/index.js'
+import { Pie, LineChart } from './lib/index.js'
 
 window.addEventListener('load', () => {
   const pie = Pie({
     target: '.pie-container',
-    size: 500,
+    size: 300,
     segments: [
       { percent: 65, color: '#05a790', legend: 'Water [%]' },
       { percent: 16, color: '#13c1a9', legend: 'Protein [%]' },
       { percent: 14, color: '#27dac1', legend: 'Fat [%]' },
       { percent: 5, color: '#3dead2', legend: 'Mineral [%]' },
+    ],
+  })
+
+  const lineChart = LineChart({
+    target: '.line-chart-container',
+    maxY: 30000,
+    intervalY: 5000,
+    data: [
+      {
+        x: '6/1',
+        y: 10000,
+      },
+      {
+        x: '6/2',
+        y: 0,
+      },
+      {
+        x: '6/3',
+        y: 5000,
+      },
+      {
+        x: '6/4',
+        y: 20000,
+      },
+      {
+        x: '6/5',
+        y: 15000,
+      },
+      {
+        x: '6/6',
+        y: 17000,
+      },
+      // {
+      //   x: '6/7',
+      //   y: 30000,
+      // },
     ],
   })
 })
